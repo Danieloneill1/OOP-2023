ec425cf5551805f551e7e2c3d395254104d880f3
Daniel O'Neill <danieloneill@Daniels-MacBook-Air.local>
2023-02-20 12:48:14 +0000

    audio1

--- java/src/ie/tudublin/audio1.java
+++ java/src/ie/tudublin/audio1.java
@@ -10,26 +10,50 @@ public class audio1 extends PApplet
 {
 
 	Minim minim;
-	AudioInput al;
+	AudioInput ai;
 	AudioPlayer ap;//microphone 
-	AudioBuffer ab;
+	AudioBuffer ab;//audio buffer like an array
 
 
 	public void settings()
 	{
-		size(500, 500);
+		size(1024, 500);//1024 samples per sec
 	}
+
+	int frameSize = 1024;
 
 	public void setup() {
-		
-	}
+		colorMode(HSB);
+		background(0);
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+		minim = new Minim(this);//create new object
+
+		ai = minim.getLineIn(minim.MONO, frameSize, 44100, 16);
+		ab = ai.mix;//left and right chanel
+
+		smooth();
+	}
 	
 	public void draw()
 	{
+		background(0);
+		stroke(255);
 
+		float half = height / 2;
+		float cgap = 255 / (float)ab.size();//255 colours
+		float average = 0;
+
+		for(int i = 0; i < ab.size(); i++)
+		{
+			abs (average = average + i);
+		}
+
+		average = average/ab.size();
+
+		for(int i = 0; i < ab.size(); i++)//iterate through each frame
+		{
+			stroke(cgap * i, 255, 255);
+			line(i, half, i, half + ab.get(i)*half);//ab.get like ab[i]
+		}
 	}
 }
